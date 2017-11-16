bd504fded3a666d26e1cda7bc8a91b5fd577c0c3
Laura Brandt <laurambrandt@gmail.com>
2017-11-16 15:42:02 -0300

    added responsive design

--- static/src/squareImages.js
+++ static/src/squareImages.js
@@ -2,3 +2,8 @@ $(document).ready(function() {
     var dimension = $('.newsimg').width();
     $('.newsimg').height(dimension);
 });
+
+$(window).resize(function() {
+    var dimension = $('.newsimg').width();
+    $('.newsimg').height(dimension);
+});
